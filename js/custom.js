0884eb62d1ce60067230c03ed573abef5fad6f12
gc20078-code <gc20078@ues.edu.sv>
2026-09-05 22:25:52 -0600

    haciendo la modificacion
    ya se pueden desplegar los target donde se añaden los productos al carrito
    de compras

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,17 @@
 
+// Variables
+let allContainerCart = document.querySelector('.products-grid');
+
+// Funcion
+loadEventListeners();
+function loadEventListeners(){
+    allContainerCart.addEventListener('click', addProduct);
+}
+
+function addProduct(e){ 
+     e.preventDefault();
+    if(e.target.classList.contains('add-to-cart')){
+        const selectProduct = e.target.parentElement;
+        console.log(selectProduct); // ← Movido DENTRO del if
+    }
+}
